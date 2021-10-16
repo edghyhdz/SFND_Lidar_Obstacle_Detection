ff80b0202334122afc2c7c23c5537483905aff88
edghyhdz <edgar.hdz@hotmail.com>
2021-10-16 14:30:59 +0200

    Added helper_insert() function - to insert nodes recursively

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -39,8 +39,26 @@ struct KdTree
 	void insert(std::vector<float> point, int id)
 	{
 		// TODO: Fill in this function to insert a new point into the tree
-		// the function should create a new node and place correctly with in the root 
+		// the function should create a new node and place correctly with in the root
+		helper_insert(&root, point, 0, id); 
+	}
 
+	/** \brief Helper function to add nodes into \b *KdTree::root
+	  * \param[in, out] node a node to be added into \b *KdTree::root
+	  * \param[in] data x or y value
+	  * \param[in] depth x or y split index
+	  * \param[in] id node id
+	  */
+	void helper_insert(Node **node, std::vector<float> data, int depth, int id){
+		if (*node == NULL) {
+			*node = new Node(data, id); 
+		}
+		else if (data[depth % 2] < (*node)->point[depth % 2]){
+			helper_insert(&(*node)->left, data, depth + 1, id); 
+		}
+		else {
+			helper_insert(&(*node)->right, data, depth + 1, id); 
+		}
 	}
 
 	// return a list of point ids in the tree that are within distance of target
