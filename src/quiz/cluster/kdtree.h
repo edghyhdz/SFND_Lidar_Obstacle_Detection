66ef9849fcbe878f0f17bd8226e66bed4892a2a4
edghyhdz <edgar.hdz@hotmail.com>
2021-10-16 14:35:04 +0200

    Added helper_search() function - search nearby nodes in KdTree::root pointer

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -61,14 +61,51 @@ struct KdTree
 		}
 	}
 
+	/** \brief Helper function to search for nearby nodes in \b *KdTree::root
+	  * \param[in] node a node \b *KdTree::root to search for points
+	  * \param[in] target x or y value
+	  * \param[in] distanceTol x or y split index
+	  * \param[in] depth node id
+	  * \param[in, out] ids node id
+	  */
+	void helper_search(Node const *node, std::vector<float> target, float distanceTol, int depth, std::vector<int> *ids){
+		std::cout << "Helper search. Depth: " << std::to_string(depth) << std::endl; 
+
+		if (node != NULL){
+			std::cout << "Checking (" << node->point[0] << ", " << node->point[1] << ")" << std::endl; 
+		} else {
+			std::cout << "NULL NODE" << std::endl; 
+		}
+
+		if (node != NULL){
+
+			if ( (node->point[0]>=(target[0] - distanceTol)&&node->point[0] <=(target[0]+distanceTol)) && (node->point[1]>=(target[1] - distanceTol)&&node->point[1] <=(target[1]+distanceTol))){
+
+			// if ((((target[0] - distanceTol) <= node->point[0]) && (node->point[0] <= (target[0] + distanceTol))) && (((target[1] - distanceTol) <= node->point[1]) && (node->point[1] <= (target[1] + distanceTol))) ){
+				// Calculate distance
+				(*ids).push_back(node->id);
+			} 
+			
+			if ((target[depth % 2] - distanceTol) < node->point[depth % 2] ){
+				depth++;
+				helper_search(node->left, target, distanceTol, depth, ids); 
+			}
+			if((target[depth % 2] + distanceTol) > node->point[depth % 2]){
+				depth++;
+				helper_search(node->right, target, distanceTol, depth, ids); 
+			}
+		}
+	}
+
 	// return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
 		std::vector<int> ids;
+		uint32_t depth{0};
+		helper_search(root, target, distanceTol, depth, &ids);
 		return ids;
 	}
 	
-
 };
 
 
